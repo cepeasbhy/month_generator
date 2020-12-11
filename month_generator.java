106080a11d3615779aab2988017387ea04b89f52
cepeasbhy <asbhycepe01@gmail.com>
2020-12-11 14:01:19 +0800

    Added fucntion to determine leap years

--- month_generator.java
+++ month_generator.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class month_generator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int year = input.nextInt();
+		int leap_year = leap(year);
+		
+		System.out.print(leap_year);
 
+	}
+	
+	public static int leap(int year) {
+		if(year%4 == 0) {
+			if(year%100==0) {
+				if(year%400 == 0) {
+					return 1;
+				}else {
+					return 0;
+				}
+			}else {
+				return 0;
+			}
+		}else {
+			return 0;
+		}
 	}
 
 }

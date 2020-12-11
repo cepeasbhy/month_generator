fadd8e523d797ca9a24bd78bb48bc248955069b7
cepeasbhy <asbhycepe01@gmail.com>
2020-12-11 14:48:12 +0800

    Added formula for year code

--- month_generator.java
+++ month_generator.java
@@ -4,11 +4,17 @@ public class month_generator {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		System.out.println("MONTH GENERATOR");
+		System.out.println("---------------");
 		
+		System.out.print("Enter year: ");
 		int year = input.nextInt();
-		int leap_year = leap(year);
 		
-		System.out.print(leap_year);
+		int last = year%100;
+		int yearcode = (last+(last/4))%7;
+		System.out.print(yearcode);
+		
+		int leap_year = leap(year);
 
 	}
 	
@@ -17,7 +23,7 @@ public class month_generator {
 			return 0;
 		}else if(year%100 != 0) {
 			return 1;
-		}else if(year%400!=0) {
+		}else if(year%400 != 0) {
 			return 0;
 		}else {
 			return 1;
